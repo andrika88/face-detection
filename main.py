aea0522b8e8604a48f456288cc7bb822c5a886db
andrika88 <andrika.andrika@klarsmile.com>
2024-10-13 00:28:29 +0700

    update main

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 import cv2
 
-frontalface = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 
-camera = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0)
 
-def face_detect(frame):
-    optimized_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    face = frontalface.detectMultiScale(optimized_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    return face
+def detect_faces(frame):
+    grayscale_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    faces = face_cascade.detectMultiScale(grayscale_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    return faces
 
-def draweer_box(frame):
-    for (x, y, w, h) in face_detect(frame):
+def draw_faces(frame):
+    for (x, y, w, h) in detect_faces(frame):
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     pass
 
 def close_camera():
-    camera.release()
+    video_capture.release()
     cv2.destroyAllWindows()
     exit()
 
 def main():
     while True:
-        _, frame = camera.read()
+        _, frame = video_capture.read()
         
-        draweer_box(frame)
+        draw_faces(frame)
 
-        cv2.imshow("Face Recognition", frame)
+        cv2.imshow("Face Detection", frame)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             close_camera()
